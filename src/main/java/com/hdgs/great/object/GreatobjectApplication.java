51f30a5c5d381ef9532579bdfd9d53c615e7ba1e
ZZZXXXCCCWXY999 <zzzxxxcccwxy999@gmail.com>
2020-03-08 20:54:18 +0800

    gradle build

---
+++ src/main/java/com/hdgs/great/object/GreatobjectApplication.java
@@ -0,0 +1,13 @@
+package com.hdgs.great.object;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreatobjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreatobjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hdgs/great/object/GreatobjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hdgs.great.object;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreatobjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
