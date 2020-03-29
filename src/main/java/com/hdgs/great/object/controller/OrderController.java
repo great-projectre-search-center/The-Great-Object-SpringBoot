c65b28abb8e10fe2ac3a8eeae34f22ca5c85dabe
ZZZXXXCCCWXY999 <zzzxxxcccwxy999@gmail.com>
2020-03-29 23:14:35 +0800

    增加了需求文档

---
+++ src/main/java/com/hdgs/great/object/controller/OrderController.java
@@ -0,0 +1,13 @@
+package com.hdgs.great.object.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+    //编辑订单 postmapping("/edit")
+    //删除订单 deletemapping("/delete")
+    //查看所有订单 getmapping("/list")
+    //根据订单id查看订单 getmapping("/list/{orderid}")girt
+}
